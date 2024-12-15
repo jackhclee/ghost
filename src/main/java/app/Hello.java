9869d20bee48bc0730e8db8cf2ca7fc7ae6f74bd
moses <moses@mosess-MacBook-Air.local>
2024-12-15 23:27:03 +0000

    fix

--- src/main/java/app/Hello.java
+++ src/main/java/app/Hello.java
@@ -7,9 +7,13 @@ public class Hello {
     Random ran = new Random();
     System.out.println(ran.nextInt());
     String script = System.getenv("SCRIPTNAME");
+    try {
     if (script != null) {
       // BAD: The script to be executed is controlled by the user.
       Runtime.getRuntime().exec(script);
+    }
+    } catch (Exception e) {
+      System.out.println(e.getStackTrace());
     }
   }
 }
